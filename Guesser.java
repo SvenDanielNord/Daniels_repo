bfce55e6ea24f4d8ad1060f159db433d29ebd6f9
Daniel Nord <svenor0701@skola.goteborg.se>
2022-11-18 09:49:33 +0100

    Update getReply()

--- Guesser.java
+++ Guesser.java
@@ -64,7 +64,7 @@ public class Guesser {
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
-    while (!reply.equalsIgnoreCase("T") && !reply.equalsIgnoreCase("F")) {
+    while (!reply.equals("T") && !reply.equals("F")) {
       System.out.println("please answer T or F");
       reply = System.console().readLine();
   
